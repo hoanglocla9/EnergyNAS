058e2f86d85c2f938aea6a1a1b12998f465bbeef
LaLoc <hla111@uit.no>
2024-09-14 18:36:08 +0200

    update source_code

--- nas/model.py
+++ nas/model.py
@@ -1,37 +1,48 @@
 import nni
 import nni.retiarii.nn.pytorch as nn
-    
+import torch
+import math
+from torch import Tensor
+
+import torch.optim
+import torch.nn.functional as F
+
+import typing as ty
+
+
 def get_activation_name_options():
     return [
-        # "__torch__.nni.retiarii.nn.pytorch.Identity", 
-        "None", ## replace for Identity
-        "__torch__.nni.retiarii.nn.pytorch.ReLU", 
-        "__torch__.nni.retiarii.nn.pytorch.Tanh", 
-        "__torch__.nni.retiarii.nn.pytorch.LeakyReLU", 
-        "__torch__.nni.retiarii.nn.pytorch.Hardshrink", 
-        "__torch__.nni.retiarii.nn.pytorch.Hardsigmoid", 
-        "__torch__.nni.retiarii.nn.pytorch.Hardtanh", 
-        "__torch__.nni.retiarii.nn.pytorch.Hardswish", 
-        "__torch__.nni.retiarii.nn.pytorch.LogSigmoid", 
-        "__torch__.nni.retiarii.nn.pytorch.PReLU", 
-        "__torch__.nni.retiarii.nn.pytorch.ELU", 
-        "__torch__.nni.retiarii.nn.pytorch.ReLU6", 
-        "__torch__.nni.retiarii.nn.pytorch.RReLU", 
+        # "__torch__.nni.retiarii.nn.pytorch.Identity",
+        "None",  # replace for Identity
+        "__torch__.nni.retiarii.nn.pytorch.ReLU",
+        "__torch__.nni.retiarii.nn.pytorch.Tanh",
+        "__torch__.nni.retiarii.nn.pytorch.LeakyReLU",
+        "__torch__.nni.retiarii.nn.pytorch.Hardshrink",
+        "__torch__.nni.retiarii.nn.pytorch.Hardsigmoid",
+        "__torch__.nni.retiarii.nn.pytorch.Hardtanh",
+        "__torch__.nni.retiarii.nn.pytorch.Hardswish",
+        "__torch__.nni.retiarii.nn.pytorch.LogSigmoid",
+        "__torch__.nni.retiarii.nn.pytorch.PReLU",
+        "__torch__.nni.retiarii.nn.pytorch.ELU",
+        "__torch__.nni.retiarii.nn.pytorch.ReLU6",
+        "__torch__.nni.retiarii.nn.pytorch.RReLU",
         "__torch__.nni.retiarii.nn.pytorch.CELU",
         "__torch__.nni.retiarii.nn.pytorch.SiLU",
         # "__torch__.nni.retiarii.nn.pytorch.Mish",
         "__torch__.nni.retiarii.nn.pytorch.Softplus",
         "__torch__.nni.retiarii.nn.pytorch.Softshrink",
         "__torch__.nni.retiarii.nn.pytorch.Softsign",
-        "__torch__.nni.retiarii.nn.pytorch.Tanhshrink" 
-    ] 
+        "__torch__.nni.retiarii.nn.pytorch.Tanhshrink"
+    ]
+
+
 @nni.retiarii.model_wrapper
 class CalibrationModelSpace(nn.Module):
-    def __init__(self, n_features=33): # , 
+    def __init__(self, n_features=33):  # ,
         super().__init__()
         layers = []
         self.n_features = n_features
-        
+
         _layers = nn.Placeholder(
             label='mutable_all',
             conv_kernel_size_options=[1, 3, 5, 7],
@@ -40,7 +51,8 @@ class CalibrationModelSpace(nn.Module):
             conv_op_type='__torch__.nni.retiarii.nn.pytorch.Conv1d',
             in_ch=n_features,
             conv_activation_fn_options=get_activation_name_options(),
-            pooling_op_type_options=["__torch__.nni.retiarii.nn.pytorch.MaxPool1d", "__torch__.nni.retiarii.nn.pytorch.AvgPool1d"],
+            pooling_op_type_options=[
+                "__torch__.nni.retiarii.nn.pytorch.MaxPool1d", "__torch__.nni.retiarii.nn.pytorch.AvgPool1d"],
             pooling_kernel_size_options=[3, 5, 7],
             flatten_options=[True, False],
             n_neurons_options=range(8, 1025, 8),
@@ -48,16 +60,18 @@ class CalibrationModelSpace(nn.Module):
             fc_op_type='__torch__.nni.retiarii.nn.pytorch.Linear',
             fc_activation_fn_options=get_activation_name_options()
         )
-        layers.append(_layers)  
+        layers.append(_layers)
         self.layers = nn.Sequential(*layers)
-                
+
     def forward(self, x):
         return self.layers(x)
-    
+
+
 def reset_weights(m):
     for layer in m.children():
         if hasattr(layer, 'reset_parameters'):
             layer.reset_parameters()
+
 
 @nni.retiarii.model_wrapper
 class MLPSpace(nn.Module):
@@ -68,28 +82,30 @@ class MLPSpace(nn.Module):
             self.activation = nn.ReLU()
             if idx == -1:
                 idx = "final"
-            self.dropout = nn.Dropout(p=nn.ValueChoice([0.1, 0.2, 0.4, 0.6, 0.8, 0.9], label="mlpblock_" + str(idx) + "_droprate"))
+            self.dropout = nn.Dropout(p=nn.ValueChoice(
+                [0.1, 0.2, 0.4, 0.6, 0.8, 0.9], label="mlpblock_" + str(idx) + "_droprate"))
+
         def forward(self, x):
             x = self.linear(x)
             x = self.activation(x)
             x = self.dropout(x)
-            return x 
-        
-    def __init__(self, n_features=33): # , 
+            return x
+
+    def __init__(self, n_features=33):  # ,
         super().__init__()
         layers = []
         self.n_features = n_features
         d_hidden = nn.ValueChoice(range(4, 1025, 4), label="d_hidden")
-        self.blocks = nn.Repeat(lambda idx: MLPSpace.MLPBlock(n_features, d_hidden, idx) if idx == 0 \
-                                        else MLPSpace.MLPBlock(d_hidden, d_hidden, idx), depth=(1, 10), label="n_mlpblocks")
-        
+        self.blocks = nn.Repeat(lambda idx: MLPSpace.MLPBlock(n_features, d_hidden, idx) if idx == 0
+                                else MLPSpace.MLPBlock(d_hidden, d_hidden, idx), depth=(1, 10), label="n_mlpblocks")
+
         self.final_block = MLPSpace.MLPBlock(d_hidden, 1)
-                
+
     def forward(self, x):
         x = self.blocks(x)
         x = self.final_block(x)
         return x
-    
+
 
 @nni.retiarii.model_wrapper
 class ResNetSpace(nn.Module):
@@ -97,32 +113,36 @@ class ResNetSpace(nn.Module):
         def __init__(self, idx, d_in=33):
             super().__init__()
             self.batchnorm_1 = nn.LayerChoice([
-                    nn.BatchNorm1d(d_in),
-                    nn.Identity()
-            ], key="resnetblock_isbn_"+str(idx))   
-            hidden_dim = nn.ValueChoice(range(4, 513, 4), label="resnetblock_" + str(idx) + "_d_hidden")
+                nn.BatchNorm1d(d_in),
+                nn.Identity()
+            ], key="resnetblock_isbn_"+str(idx))
+            hidden_dim = nn.ValueChoice(
+                range(4, 513, 4), label="resnetblock_" + str(idx) + "_d_hidden")
             self.linear_1 = nn.Linear(d_in, hidden_dim)
             self.relu_1 = nn.ReLU()
-            self.dropout_1 = nn.Dropout(p=nn.ValueChoice([0.1, 0.2, 0.4, 0.6, 0.8, 0.9], label="resnetblock_" + str(idx) + "_droprate_0"))
+            self.dropout_1 = nn.Dropout(p=nn.ValueChoice(
+                [0.1, 0.2, 0.4, 0.6, 0.8, 0.9], label="resnetblock_" + str(idx) + "_droprate_0"))
             self.linear_2 = nn.Linear(hidden_dim, d_in)
-            self.dropout_2 = nn.Dropout(p=nn.ValueChoice([0.1, 0.2, 0.4, 0.6, 0.8, 0.9], label="resnetblock_" + str(idx) + "_droprate_1"))
+            self.dropout_2 = nn.Dropout(p=nn.ValueChoice(
+                [0.1, 0.2, 0.4, 0.6, 0.8, 0.9], label="resnetblock_" + str(idx) + "_droprate_1"))
 
         def forward(self, x):
             x_input = x.squeeze(1)
-            x = self.batchnorm_1(x.squeeze(dim=1)) 
+            x = self.batchnorm_1(x.squeeze(dim=1))
             x = self.linear_1(x)
             x = self.relu_1(x)
-            x = self.dropout_1(x) 
+            x = self.dropout_1(x)
             x = self.linear_2(x)
             x = self.dropout_2(x)
-            return x_input + x 
+            return x_input + x
+
     class ResNetHead(nn.Module):
         def __init__(self, d_in, d_out):
             super().__init__()
             self.batchnorm = nn.LayerChoice([
-                    nn.BatchNorm1d(d_in),
-                    nn.Identity()
-            ], key="resnethead_isbn")   
+                nn.BatchNorm1d(d_in),
+                nn.Identity()
+            ], key="resnethead_isbn")
             self.relu = nn.ReLU()
             self.linear = nn.Linear(d_in, d_out)
 
@@ -131,88 +151,99 @@ class ResNetSpace(nn.Module):
             x = self.relu(x)
             x = self.linear(x)
             return x
-            
-    def __init__(self, n_features=33): # , 
+
+    def __init__(self, n_features=33):  # ,
         super().__init__()
         d_main = nn.ValueChoice(range(4, 257, 4), label="resnet_d_main")
         self.first_layer = nn.Linear(n_features, d_main)
-        self.resnetblocks = nn.Repeat(lambda idx: ResNetSpace.ResNetBlock(idx, d_main), (1, 10), label="n_resnetblocks")
+        self.resnetblocks = nn.Repeat(lambda idx: ResNetSpace.ResNetBlock(
+            idx, d_main), (1, 10), label="n_resnetblocks")
         self.head = ResNetSpace.ResNetHead(d_main, 1)
 
     def forward(self, x):
         x = self.first_layer(x)
-        x = self.resnetblocks (x)
+        x = self.resnetblocks(x)
         x = self.head(x)
         return x
-    
-def reset_weights(m):
-    for layer in m.children():
-        if hasattr(layer, 'reset_parameters'):
-            layer.reset_parameters()
 
 
-# @nni.retiarii.model_wrapper
-# class ResNetSpace(nn.Module):
-#     class ResNetBlock(nn.Module):
-#         def __init__(self, idx, d_in=33):
-#             super().__init__()
-#             _layers = nn.Placeholder(
-#                 label='mutable_resnetblock_{idx}',
-#                 useBN=[True, False],
-#                 dropout_1_options=[0.1, 0.2, 0.4, 0.6, 0.8, 0.9],
-#                 dropout_2_options=[0.1, 0.2, 0.4, 0.6, 0.8, 0.9],
-#                 d_hidden_options=nn.ValueChoice(range(4, 5013, 4))
-#             )
-#             layers.append(_layers)  
-#             self.layers = nn.Sequential(*layers)
-                
-#         def forward(self, x):
-#             return self.layers(x)
+class Tokenizer(nn.Module):
+    def __init__(
+        self,
+        d_numerical: int,
+        d_token: int
+    ) -> None:
+        super().__init__()
+        d_bias = d_numerical
 
-#         def forward(self, x):
-#             x_input = x.squeeze(1)
-#             x = self.batchnorm_1(x.squeeze(dim=1)) 
-#             x = self.linear_1(x)
-#             x = self.relu_1(x)
-#             x = self.dropout_1(x) 
-#             x = self.linear_2(x)
-#             x = self.dropout_2(x)
-#             return x_input + x 
+        # take [CLS] token into account
+        self.weight = nn.Parameter(Tensor(d_numerical + 1, d_token))
+        self.bias = nn.Parameter(Tensor(d_bias, d_token))
+        # The initialization is inspired by nn.Linear
+        nn.init.kaiming_uniform_(self.weight, a=math.sqrt(5))
+        nn.init.kaiming_uniform_(self.bias, a=math.sqrt(5))
 
-#     class ResNetHead(nn.Module):
-#         def __init__(self, d_in, d_out):
-#             super().__init__()
+    @property
+    def n_tokens(self) -> int:
+        return len(self.weight)
 
-#             _layers = nn.Placeholder(
-#                 label='mutable_resnethead',
-#                 useBN=[True, False]
-#             )
-#             layers.append(_layers)  
-#             self.layers = nn.Sequential(*layers)
+    def forward(self, x_num: Tensor) -> Tensor:
+        x = self.weight[:-1] * x_num[:, :]
 
-#         def forward(self, x):
-#             # x = self.batchnorm(x.squeeze(dim=1))
-#             # x = self.relu(x)
-#             # x = self.linear(x)
-#             return self.layers(x)
-            
-#     def __init__(self, n_features=33): # , 
-#         super().__init__()
-#         d_main = nn.ValueChoice(range(4, 257, 4))
-#         _layers = nn.Placeholder(
-#                 label='mutable_resnet',
-#                 n_blocks=range(1, 10),
-#                 d_main_options=range(4, 257,4)
-#             )
-#         layers.append(_layers)  
-#         self.layers = nn.Sequential(*layers)
-#         # self.first_layer = nn.Linear(n_features, d_main)
-#         # self.resnetblocks = nn.Repeat(lambda idx: ResNetSpace.ResNetBlock(idx, d_main), (1, 10))
-#         # self.head = ResNetSpace.ResNetHead(d_main, 1)
+        x = torch.cat(
+            [x, self.weight[-1].repeat(len(x), 1, 1)],
+            dim=1,
+        )
+        bias = torch.cat(
+            [
+                self.bias,
+                torch.zeros(
+                    1, self.bias.shape[1]),
+            ]
+        )
+        x = x + bias
+        return x
 
-#     def forward(self, x):
-#         return self.layers(x)
-    
+
+@nni.retiarii.model_wrapper
+class FTTransformerSpace(nn.Module):
+    def __init__(self, d_numerical, d_token=192, d_ffn_factor=4/3, n_heads=4, d_out=1):
+        super().__init__()
+        self.tokenizer = Tokenizer(
+            d_numerical, d_token
+        )
+        # n_tokens = self.tokenizer.n_tokens + 1
+        d_token = 192  # nn.ValueChoice([64, 128, 192, 256], label="d_token")
+        # d_ffn_factor = nn.ValueChoice([4/3, 1/2, 3/4, 1], label="d_ffn_factor")
+        d_hidden = d_token  # * d_ffn_factor
+        n_layers = nn.ValueChoice(range(2, 6), label="n_layers")
+        layer_dropout_rate = nn.ValueChoice(
+            [0.0, 0.1, 0.2, 0.3, 0.4], label="transformer_dropout_rate")
+        activation = nn.GELU()  # "__torch__.nni.retiarii.nn.pytorch.ReLU"  # F.gelu
+        prenormalization = False
+        encoder_layer = nn.TransformerEncoderLayer(
+            d_model=d_token, nhead=n_heads, dim_feedforward=d_hidden, dropout=layer_dropout_rate,
+            activation=activation, norm_first=prenormalization)
+        # self.encoder = nn.TransformerEncoder(
+        #     encoder_layer, num_layers=n_layers, norm=nn.LayerNorm(d_token))
+        self.encoder = encoder_layer
+        self.last_activation = F.relu
+        if prenormalization:
+            self.last_normalization = nn.LayerNorm(d_token)
+        else:
+            self.last_normalization = nn.Identity()
+        self.head = nn.Linear(d_token, d_out)
+
+    def forward(self, x):
+        x = self.tokenizer(x)
+        x = self.encoder(x)
+        x = self.last_normalization(x)
+        x = self.last_activation(x)
+        x = self.head(x)
+        x = x.squeeze(-1)
+        return x
+
+
 def reset_weights(m):
     for layer in m.children():
         if hasattr(layer, 'reset_parameters'):
--- run_nas.py
+++ run_nas.py
@@ -3,25 +3,38 @@ from nni.retiarii.evaluator import FunctionalEvaluator
 from nni.retiarii.experiment.pytorch import RetiariiExperiment, RetiariiExeConfig
 from nas.learning_utils import evaluate_model, evaluate_model_darts
 from nas.estimator import HardwareMetricFilter
-from nas.model import  MLPSpace, ResNetSpace
+from nas.model import MLPSpace, ResNetSpace, FTTransformerSpace
 
-import logging, argparse, os
+import logging
+import argparse
+import os
 _logger = logging.getLogger(__name__)
 os.environ['PICKLE_SIZE_LIMIT'] = str(10*1024*1024*1024)
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Just an example", formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    parser.add_argument("-s", "--strategy", type=str, help="search stragegy, currently, only supported for random and evolution strategies", default="random")
-    parser.add_argument("-r", "--lag_range", type=int, help="number of lag features", default=26)
-    parser.add_argument("-n", "--trial_number", type=int, help="number of trial steps", default=20)
-    parser.add_argument("-c", "--n_gpus", type=int, help="number of gpus", default=0)
-    parser.add_argument("-p", "--port", type=int, help="NNI WebUI Port", default=8081)
-    parser.add_argument("-t", "--target", type=str, help="Training Label Name", default="ref_ch4(ppm)")
-    parser.add_argument("-me", "--metrics", type=list, help="The Optimized Metric", default=["MPAE", "energy"])
-    parser.add_argument("-m", "--mode", type=str, help="Constraint or MOO mode", default="debug")
-    parser.add_argument("-th", "--target_hardware", type=str, help="Target hardware", default="myriadvpu_openvino2019r2")
-    parser.add_argument("-bm", "--backbone_model", type=str, help="The base model of NAS", default="mlp")
-    
+    parser = argparse.ArgumentParser(
+        description="Just an example", formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument("-s", "--strategy", type=str,
+                        help="search stragegy, currently, only supported for random and evolution strategies", default="random")
+    parser.add_argument("-r", "--lag_range", type=int,
+                        help="number of lag features", default=26)
+    parser.add_argument("-n", "--trial_number", type=int,
+                        help="number of trial steps", default=20)
+    parser.add_argument("-c", "--n_gpus", type=int,
+                        help="number of gpus", default=0)
+    parser.add_argument("-p", "--port", type=int,
+                        help="NNI WebUI Port", default=8081)
+    parser.add_argument("-t", "--target", type=str,
+                        help="Training Label Name", default="ref_ch4(ppm)")
+    parser.add_argument("-me", "--metrics", type=list,
+                        help="The Optimized Metric", default=["MPAE", "energy"])
+    parser.add_argument("-m", "--mode", type=str,
+                        help="Constraint or MOO mode", default="debug")
+    parser.add_argument("-th", "--target_hardware", type=str,
+                        help="Target hardware", default="myriadvpu_openvino2019r2")
+    parser.add_argument("-bm", "--backbone_model", type=str,
+                        help="The base model of NAS", default="mlp")
+
     thresholds = {"latency": 5, "accuracy": 0.3}
 
     args = parser.parse_args()
@@ -29,48 +42,58 @@ if __name__ == "__main__":
     if "energy" in cfg['metrics'] and "latency" in cfg['metrics']:
         cfg['metrics'].remove("latency")
     if cfg['backbone_model'] == 'mlp':
-        model_space = MLPSpace(n_features=cfg['lag_range']+7)                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                            
-    else:
+        model_space = MLPSpace(n_features=cfg['lag_range']+7)
+    elif cfg['backbone_model'] == 'resnet':
         model_space = ResNetSpace(n_features=cfg['lag_range']+7)
-
-
-    evaluator = FunctionalEvaluator(evaluate_model, lag_range=cfg['lag_range'], 
-                                        target=cfg['target'], 
-                                        optimized_metrics=cfg['metrics'], 
-                                        mode=cfg['mode'],
-                                        target_values=thresholds) 
-    
-    if cfg['mode'] == "filter":
-        model_filter = HardwareMetricFilter(thresholds, applied_hardware=cfg['target_hardware'], reverse=False)
+    elif cfg['backbone_model'] == 'fttransformer':
+        model_space = FTTransformerSpace(d_numerical=cfg['lag_range']+7)
     else:
-        model_filter = None 
+        raise Exception('Not support this backbone model yet!')
+
+    evaluator = FunctionalEvaluator(evaluate_model, lag_range=cfg['lag_range'],
+                                    target=cfg['target'],
+                                    optimized_metrics=cfg['metrics'],
+                                    mode=cfg['mode'],
+                                    target_values=thresholds)
+
+    if cfg['mode'] == "filter":
+        model_filter = HardwareMetricFilter(
+            thresholds, applied_hardware=cfg['target_hardware'], reverse=False)
+    else:
+        model_filter = None
 
     if cfg["strategy"] == "random":
-        search_strategy = strategy.Random(dedup=True, model_filter=model_filter)
+        search_strategy = strategy.Random(
+            dedup=True, model_filter=model_filter)
     elif cfg["strategy"] == "evolution":
-        search_strategy = strategy.RegularizedEvolution(optimize_mode="maximize", 
-                                                        sample_size = cfg["trial_number"]//8, 
-                                                        population_size=cfg["trial_number"]//2, 
+        search_strategy = strategy.RegularizedEvolution(optimize_mode="maximize",
+                                                        sample_size=cfg["trial_number"]//8,
+                                                        population_size=cfg["trial_number"]//2,
                                                         cycles=cfg["trial_number"],
                                                         model_filter=model_filter
-                                                    )
+                                                        )
     elif cfg["strategy"] == "reinforce":
         if cfg["trial_number"] >= 20:
-            search_strategy = strategy.PolicyBasedRL(max_collect=cfg["trial_number"]//20, trial_per_collect=20)
+            search_strategy = strategy.PolicyBasedRL(
+                max_collect=cfg["trial_number"]//20, trial_per_collect=20)
         else:
-            search_strategy = strategy.PolicyBasedRL(max_collect=cfg["trial_number"]//2, trial_per_collect=2)
+            search_strategy = strategy.PolicyBasedRL(
+                max_collect=cfg["trial_number"]//2, trial_per_collect=2)
     elif cfg["strategy"] == "darts":
         search_strategy = strategy.DARTS()
-        evaluator = evaluate_model_darts(lag_range=cfg['lag_range'], target=cfg['target'], n_gpus=cfg['n_gpus'], max_epochs=50, fast_dev_run=False)
+        evaluator = evaluate_model_darts(
+            lag_range=cfg['lag_range'], target=cfg['target'], n_gpus=cfg['n_gpus'], max_epochs=50, fast_dev_run=False)
 
     exp = RetiariiExperiment(model_space, evaluator, [], search_strategy)
     exp_config = RetiariiExeConfig('local')
     exp_config.experiment_name = 'mnist_search'
     exp_config.execution_engine = 'base' if cfg["strategy"] != "darts" else 'oneshot'
-    exp_config.max_trial_number = cfg["trial_number"]   # spawn 4 trials at most
+    # spawn 4 trials at most
+    exp_config.max_trial_number = cfg["trial_number"]
     exp_config.experiment_working_directory = "./nni-experiments/"
     if cfg["n_gpus"] > 0:
-        exp_config.trial_concurrency = cfg["n_gpus"]# will run two trials concurrently
+        # will run two trials concurrently
+        exp_config.trial_concurrency = cfg["n_gpus"]
         exp_config.trial_gpu_number = cfg["n_gpus"]
         exp_config.training_service.use_active_gpu = True
     else:
@@ -84,7 +107,8 @@ if __name__ == "__main__":
         trimmed_target = "ch4"
     else:
         trimmed_target = "h2o"
-    folder_path = 'results/{}_{}_{}_{}'.format(cfg["strategy"], trimmed_target, "-".join(cfg['metrics']), cfg["trial_number"])
+    folder_path = 'results/{}_{}_{}_{}'.format(
+        cfg["strategy"], trimmed_target, "-".join(cfg['metrics']), cfg["trial_number"])
     if not os.path.exists(folder_path):
         os.makedirs(folder_path)
 
